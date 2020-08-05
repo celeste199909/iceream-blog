695156510384fda25a6b8de3dab9a178ba204cdf
iceream <mac@macdeMacBook-Pro.local>
2020-08-05 23:11:04 +0800

    添加左侧的菜单按钮,以及筛选功能

--- client/js/daynight.js
+++ client/js/daynight.js
@@ -4,6 +4,13 @@ let btn = document.querySelector(".day-night")
 let body = document.querySelector("body");
 let nav = document.querySelector("#nav");
 let main = document.querySelector("#main");
+// 手机Chrome浏览器上方变色
+let theme = document.querySelector("meta[name=theme-color]");
+// 菜单变色
+// let menu = document.querySelector("#menu");
+// console.log(theme);
+let banner = document.querySelector(".banner");
+// console.log(banner);
 
 window.onload = function () {
     reload()
@@ -15,28 +22,60 @@ function reload() {
         body.style.setProperty("background-color", "#eff3f6");
         nav.style.setProperty("background-color", "#fff");
         main.style.setProperty("background-color", "#fff");
+        // 手机Chrome浏览器上方变色
+        theme.setAttribute("content", "#fff");
+        // 菜单变色
+        menu.style.setProperty("background-color", "#fff");
+        // banner背景图切换
+        // banner.style.setProperty("background-image", "url(../images/banner-day-min.jpg)");
     } else if (isNight === "Yes") {
         body.style.setProperty("color", "#ddd");
         body.style.setProperty("background-color", "#708090");
         nav.style.setProperty("background-color", "#353d46");
         main.style.setProperty("background-color", "#353d46");
+        // 手机Chrome浏览器上方变色
+
+        theme.setAttribute("content", "#353d46");
+        // 菜单变色
+        menu.style.setProperty("background-color", "#353d46");
+        // banner背景图切换
+        // banner.style.setProperty("background-image", "url(../images/banner-night-min.jpg)");
     }
 }
 
 // location.reload()
 function changeStyle() {
     let isNight = localStorage.getItem("isNight");
-    if (isNight === "Yes" || isNight === null) {
+    if (isNight === "Yes") {
+        // 判断当前状态是夜晚，则点击时变成白天
+
         body.style.setProperty("color", "#000");
         body.style.setProperty("background-color", "#eff3f6");
+
         nav.style.setProperty("background-color", "#fff");
         main.style.setProperty("background-color", "#fff");
+
+        theme.setAttribute("content", "#fff");
+
+        menu.style.setProperty("background-color", "#fff");
+        // banner背景图切换
+        // banner.style.setProperty("background-image", "url(../images/banner-day-min.jpg)");
+
+        // 并且把状态改成白天
         localStorage.setItem("isNight", "No")
-    } else if (isNight === "No") {
+    } else if (isNight === "No" || isNight === null) {
+        // 判断当前状态不是夜晚，则点击时变成夜晚
+
         body.style.setProperty("color", "#ddd");
         body.style.setProperty("background-color", "#708090");
         nav.style.setProperty("background-color", "#353d46");
         main.style.setProperty("background-color", "#353d46");
+
+        theme.setAttribute("content", "#353d46");
+        menu.style.setProperty("background-color", "#353d46");
+        // banner背景图切换
+        // banner.style.setProperty("background-image", "url(../images/banner-night-min.jpg)");
+        // 并且把当前状态改成夜晚
         localStorage.setItem("isNight", "Yes")
     }
 }
--- client/js/index.js
+++ client/js/index.js
@@ -1,22 +1,71 @@
-// 创建练习项
-// function renderPracticesList(arr) {
-//     arr.forEach(item => {
-//         let ul = document.querySelector("ul");
-//         let li = document.createElement("li");
-//         li.classList.add("practice-item");
-//         li.innerHTML = `<div class="preview">
-//                             <img src="${item.preview}" alt="" >
-//                         </div>
-//                         <div class="item-right">
-//                             <a class="name" href="${item.link}">${item.name}</a>
-//                             <div class="profile">${item.profile}</div>
-//                             <div class="tags"></div>
-//                         </div>`
-//         ul.appendChild(li)
-//         tags = item.tags;
-//     });
-// }
-// renderPracticesList(practices);
+// localStorage.setItem("practices", JSON.stringify(practices));
+
+
+// 渲染li
+function renderPracticesList(practices) {
+    let ul = document.querySelector(".content");
+    ul.innerHTML = "";
+    practices.forEach(item => {
+        let li = document.createElement("li");
+        li.classList.add("content-item");
+        li.innerHTML = `
+            <a class="title" href="${item.link}">${item.title}</a>
+            <p class="profile">${item.profile}</p>
+            <p class="preview">
+                <img class="preview-img" src="${item.preview}" alt="">
+            </p>
+            <div class="foot">
+                <div class="foot-item">${item.date}</div>
+                <div class="foot-item tags">
+                    <span class="tag">${item.category}</span>
+                </div>
+            </div>
+        `
+        ul.appendChild(li)
+    });
+}
+
+// 过滤器，根据分类过滤，得到一个新数组
+function filterCate(practices, cate) {
+    return practices.filter((item) => {
+        return item.category === cate;
+    })
+}
+// 触发点击事件，渲染过滤后的数组
+let cate = document.querySelector(".cate");
+cate.addEventListener("click", (e) => {
+    let category = e.target.innerText;
+    let newArr = filterCate(practices, category);
+    // console.log("hhh", newArr);
+    // localStorage.setItem("practices", JSON.stringify(newArr))
+    renderPracticesList(newArr);
+
+})
+// 默认加载全部
+// practices = JSON.parse(localStorage.getItem("practices"))
+renderPracticesList(practices);
+
+
+
+
+
+
+// console.log(JSON.parse(localStorage.getItem("newArr")));
+
+// 菜单显示/隐藏
+let menuBtn = document.querySelector(".logo");
+let menu = document.querySelector("menu");
+menuBtn.addEventListener("click", () => {
+    let isShow = menu.style.getPropertyValue("display");
+    // transition: ;
+    // console.log(isShow);
+    if (isShow === "block")
+        menu.style.display = "none";
+    else
+        menu.style.display = "block";
+})
+
+
 
 
 
