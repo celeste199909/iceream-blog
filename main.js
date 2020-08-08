4f079db944c2a69136cfbd3b334ee321f23787a6
iceream <1598474045@qq.com>
2020-08-08 21:33:14 +0800

    0.02

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
 const koa = require("koa")
 const Router = require("koa-router")
 const static = require("koa-static")
+const bodyParser = require("koa-bodyparser")
 const fs = require("fs")
-const app2 = require("./admin.js")
 
 const app = new koa()
 const router = new Router()
 
-
 router.get("/", (ctx) => {
-    // console.log("hello")
     ctx.render("index.html")
 })
-// router.get("/data/:name", (ctx) => {
-//     console.log(ctx.url);
-//     ctx.res.setHeader("Content-Type", "text/plain;charset=utf-8")
-//     let practices = fs.readFileSync(__dirname + ctx.url).toString()
-//     console.log(practices);
-//     ctx.res.write(practices)
-// })
+// 管理员，登录
+router.get("/login", async (ctx) => {
+    // await ctx.res.write("/admin/login.html")
+    ctx.body = "kk"
+    ctx.res.setHeader("Content-type", "text/html;charset=utf-8")
+    let content = fs.readFileSync(__dirname + "/client/admin/login.html")
+    // console.log(content);
+    // ctx.res.write(content)
+    ctx.body = content
 
-// router.get("/detail/:name", (ctx) => {
-//     // console.log("detail");
-//     console.log(ctx.url);
-//     // url = 
-//     // ctx.render("detail.html")
-//     ctx.redirect(`/practices/${name}`)
-// })
-// router2.get("/", (ctx) => {
-//     ctx.body = 'Administrator'
-// })
+})
+// 当表单提交时，处理表单，判断密码是否正确，正确则进入manage页面，否则进入error页面
+router.post("/handlelogin", (ctx) => {
+    if (ctx.request.body.password === "123") {
+        console.log("密码正确");
+        // 密码正确是设置cookie，存留60分钟
+        ctx.cookies.set("isLogin", "Yes", {
+            maxAge: 1000 * 60 * 60
+        })
+        ctx.redirect("/manage")
+    } else {
+        console.log("密码错误");
+        ctx.redirect("/error")
+    }
+})
+router.get("/error", (ctx) => {
+    ctx.res.setHeader("Content-type", "text/html;charset=utf-8")
+    let content = fs.readFileSync(__dirname + "/client/admin/error.html")
+    ctx.body = content
+})
+router.get("/manage", (ctx) => {
+    let loginStatus = ctx.cookies.get("isLogin")
+    if (loginStatus === "Yes") {
+        ctx.res.setHeader("Content-type", "text/html;charset=utf-8")
+        let content = fs.readFileSync(__dirname + "/client/admin/manage.html")
+        ctx.body = content
+    } else {
+        ctx.redirect("/login")
+    }
+})
 
 app.use(static(__dirname + "/client"))
-app.use(static(__dirname + "/data"))
+
+app.use(bodyParser())
 
 app.use(router.routes())
 
