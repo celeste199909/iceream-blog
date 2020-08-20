887739e1370b97bb57cab24cd5aa303547205f2a
iceream <1598474045@qq.com>
2020-08-20 15:23:05 +0800

    Update model.js
    隐藏密码

--- server/models/model.js
+++ server/models/model.js
@@ -1,14 +1,9 @@
 const mysql2 = require("mysql2")
 
 let db = mysql2.createConnection({
-    host: "112.124.18.251",
-    port: "3306",
-    database: "mysite",
-    user: "root",
-    password: "mysql1099"
     // host: "localhost",
     // port: "3306",
-    // database: "mysite",
+    // database: "test",
     // user: "root",
     // password: ""
 })
